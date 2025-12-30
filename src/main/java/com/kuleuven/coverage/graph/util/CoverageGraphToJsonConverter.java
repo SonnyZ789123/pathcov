2e73d47a8051c241cfb8dde5e0470248f4c84728
SonnyZ789123 <yoran.mertens@telenet.be>
2025-12-30 11:06:44 +0100

    Add covered field to coverage graph json export

--- src/main/java/com/kuleuven/coverage/graph/util/CoverageGraphToJsonConverter.java
+++ src/main/java/com/kuleuven/coverage/graph/util/CoverageGraphToJsonConverter.java
@@ -10,7 +10,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class CoverageGraphToJsonConverter {
-    private record Node(String id, String label, double weight) {}
+    private record Node(String id, String label, boolean covered, double weight) {}
 
     private record Edge(String from, String to, String label, double weight, Integer branchIdx) {}
 
@@ -34,6 +34,7 @@ public class CoverageGraphToJsonConverter {
                     Node jsonNode = new Node(
                             String.valueOf(node.getBlockInfo().blockId()),
                             node.getBlock().getTail().toString(),
+                            node.getCoverageCount() > 0,
                             node.getCoverageCount()
                     );
 
