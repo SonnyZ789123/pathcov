4b9065954c70823c20badf858b0b28191f798247
SonnyZ789123 <yoran.mertens@telenet.be>
2026-01-07 09:41:08 +0100

    Add methodFullName to CoveragePath

--- src/main/java/com/kuleuven/coverage/CoverageAgent/shared/CoveragePath.java
+++ src/main/java/com/kuleuven/coverage/CoverageAgent/shared/CoveragePath.java
@@ -1,10 +1,12 @@
 package com.kuleuven.coverage.CoverageAgent.shared;
 
 public final class CoveragePath implements java.io.Serializable {
+    public final String methodFullName;
     public final int[] instructionPath;
     public final int[] blockPath;
 
-    public CoveragePath(int[] instructionPath, int[] blockPath) {
+    public CoveragePath(String methodFullName, int[] instructionPath, int[] blockPath) {
+        this.methodFullName = methodFullName;
         this.instructionPath = instructionPath;
         this.blockPath = blockPath;
     }
