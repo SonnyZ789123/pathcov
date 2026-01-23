1a65b6a8127c90cd7287997ce98eb1c21348c482
SonnyZ789123 <yoran.mertens@telenet.be>
2026-01-23 13:01:58 +0100

    chore: remove simple mapping DTOs

--- src/main/java/com/kuleuven/coverage/intellij/model/coverage/ClassCoverage.java
+++
@@ -1,9 +0,0 @@
-package com.kuleuven.coverage.intellij.model.coverage;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class ClassCoverage {
-    public String name;
-    public List<MethodCoverage> methods = new ArrayList<>();
-}
--- src/main/java/com/kuleuven/coverage/intellij/model/coverage/MethodCoverage.java
+++
@@ -1,9 +0,0 @@
-package com.kuleuven.coverage.intellij.model.coverage;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class MethodCoverage {
-    public String methodSignature;
-    public List<LineCoverage> lines = new ArrayList<>();
-}
